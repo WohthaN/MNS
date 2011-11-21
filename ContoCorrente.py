dc133503ccb2a1a39e3d295cef1460d1bcf2b360
Samuele Carli <samuele.carli@cern.ch>
2011-11-21 21:09:09 +0100

    REMOVED SHELL INTERACTION

--- ContoCorrente.py
+++ ContoCorrente.py
@@ -6,9 +6,6 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy
 
-def main(operationsArray, interestRate, startValue):
-    pass
-
 def recursiveMethod(opA, iR, sV):
     y = [sV]
     for i in xrange(len(opA)):
@@ -39,7 +36,7 @@ axs[0][0].plot(xrange(len(iy)), iy, 'b-')
 axs[0][0].plot(xrange(len(riy)), riy, 'bx')
 axs[0][0].grid()
 axs[0][0].legend(('depositi','m. ricorsivo','m. risolutivo'))
-
+axs[0][0].title.set_text('Conto senza interessi')
 
 (ix,iy) = recursiveMethod(opAc, iR, sV)
 riy = [resolutiveMethod(opAc[0:x],iR,sV) for x in xrange(steps+1)]
@@ -48,6 +45,7 @@ axs[0][1].plot(xrange(len(iy)), iy, 'g-')
 axs[0][1].plot(xrange(len(riy)), riy, 'gx')
 axs[0][1].grid()
 axs[0][1].legend(('depositi','m. ricorsivo','m. risolutivo'))
+axs[0][1].title.set_text('Conto senza interessi')
 
 (ix,iy) = recursiveMethod(opA, iR1, sV)
 riy = [resolutiveMethod(opA[0:x],iR1,sV) for x in xrange(steps+1)]
@@ -55,6 +53,7 @@ axs[1][0].plot(xrange(len(ix)), ix, 'b-o')
 axs[1][0].plot(xrange(len(iy)), iy, 'b-')
 axs[1][0].plot(xrange(len(riy)), riy, 'bx')
 axs[1][0].grid()
+axs[1][0].title.set_text('Conto con interessi')
 
 (ix,iy) = recursiveMethod(opAc, iR1, sV)
 riy = [resolutiveMethod(opAc[0:x],iR1,sV) for x in xrange(steps+1)]
@@ -62,23 +61,6 @@ axs[1][1].plot(xrange(len(ix)), ix, 'g-o')
 axs[1][1].plot(xrange(len(iy)), iy, 'g-')
 axs[1][1].plot(xrange(len(riy)), riy, 'gx')
 axs[1][1].grid()
+axs[1][1].title.set_text('Conto con interessi')
 
 pylab.show()
-#
-#if __name__ == "__main__":
-#
-#    parser = argparse.ArgumentParser()
-#    parser.add_argument("-s", "--start-value", action="store", dest="opA", required=True,
-#                        type=float)
-#    parser.add_argument("-i", "--interest-rate", action="store", dest="interests", required=True,
-#                        type=float)
-#    parser.add_argument("-o", "--deposits", action="store", dest="opl", default="[]")
-#    options = parser.parse_args()
-#    opA = options.opA
-#    interests = options.interests
-#    opl = eval(options.opl)
-#
-#    recorsiveMethod(opl, interests, opA)
-#    resolutiveMethod(opl, interests, opA)
-#
-#    print opA, interests, opl
