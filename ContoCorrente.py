14a2415c5421b1ce5d56a7e0c418e9c29f83151e
Samuele Carli <samuele.carli@cern.ch>
2011-11-21 18:13:28 +0100

    labels

--- ContoCorrente.py
+++ ContoCorrente.py
@@ -26,35 +26,42 @@ fig, axs = plt.subplots(2, 2, sharex=True, sharey=True)
 
 steps = 12
 
-opA = [random.random()*50 for i in xrange(steps)]
+opA = [random.gauss(1,2)*25 for i in xrange(steps)]
 opAc = [25 for i in xrange(steps)]
 iR = 1
-iR1 = 1.15
-sV = 100
+iR1 = 1.1
+sV = 10
 
 (ix,iy) = recursiveMethod(opA, iR, sV)
 riy = [resolutiveMethod(opA[0:x],iR,sV) for x in xrange(steps+1)]
 axs[0][0].plot(xrange(len(ix)), ix, 'b-o')
 axs[0][0].plot(xrange(len(iy)), iy, 'b-')
 axs[0][0].plot(xrange(len(riy)), riy, 'bx')
+axs[0][0].grid()
+axs[0][0].legend(('depositi','m. ricorsivo','m. risolutivo'))
+
 
 (ix,iy) = recursiveMethod(opAc, iR, sV)
 riy = [resolutiveMethod(opAc[0:x],iR,sV) for x in xrange(steps+1)]
 axs[0][1].plot(xrange(len(ix)), ix, 'g-o')
 axs[0][1].plot(xrange(len(iy)), iy, 'g-')
 axs[0][1].plot(xrange(len(riy)), riy, 'gx')
+axs[0][1].grid()
+axs[0][1].legend(('depositi','m. ricorsivo','m. risolutivo'))
 
 (ix,iy) = recursiveMethod(opA, iR1, sV)
 riy = [resolutiveMethod(opA[0:x],iR1,sV) for x in xrange(steps+1)]
 axs[1][0].plot(xrange(len(ix)), ix, 'b-o')
 axs[1][0].plot(xrange(len(iy)), iy, 'b-')
 axs[1][0].plot(xrange(len(riy)), riy, 'bx')
+axs[1][0].grid()
 
 (ix,iy) = recursiveMethod(opAc, iR1, sV)
 riy = [resolutiveMethod(opAc[0:x],iR1,sV) for x in xrange(steps+1)]
 axs[1][1].plot(xrange(len(ix)), ix, 'g-o')
 axs[1][1].plot(xrange(len(iy)), iy, 'g-')
 axs[1][1].plot(xrange(len(riy)), riy, 'gx')
+axs[1][1].grid()
 
 pylab.show()
 #
