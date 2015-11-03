02b644da0d99e91f3e9a1007c73999d4f0850a95
Samuele Carli <carlisamuele@csspace.net>
2015-11-03 12:59:08 +0100

    Revert "fix"
    This reverts commit da80a2cca9fc4a4b384fdb06f666fbc8beee68b5.

--- 02-arrotondamento-fibonacci-pert.py
+++ 02-arrotondamento-fibonacci-pert.py
@@ -25,20 +25,11 @@ print "Cinv_x_C:\n", Cinv_x_C
 print("Coefficienti 64 bit:\na1: %s\na2: %s\n%s" % (float_format(a1_64), float_format(a2_64), type(a2_64)))
 print "\n"
 
-z1 = (1+np.sqrt(np.float32(5)))/2
-z2 = (1-np.sqrt(np.float32(5)))/2
-c0 = np.float64(1.)
-c1 = (1-np.sqrt(np.float64(5)))/2
-C = np.array([[1., 1.],
-             [z1, z2]]).astype('float32')
-c = np.array([c0, c1]).astype('float32')
-Cinv = np.array([[-z2/(z1-z2), 1/(z1-z2)],
-                    [-z1/(z2-z1), 1/(z2-z1)]]).astype('float32')
-Cinv_x_C = np.dot(Cinv, C).astype('float32')
+C = C.astype('float32')
+c = c.astype('float32')
+Cinv.astype('float32')
 a1_32, a2_32 = np.dot(Cinv, c).astype('float32')
 
-def y32(n):
-    return  a1_32 * np.float32((1+sqrt(5))/2) ** n - a2_32 * np.float32((1-sqrt(5))/2) ** n
 
 print "32 bit\n",
 print "z1=%s, z2=%s" % (float_format(z1),float_format(z2))
@@ -48,6 +39,11 @@ print "Cinv:\n", Cinv
 print "Cinv_x_C:\n", Cinv_x_C
 print("Coefficienti 32 bit:\na1: %s\na2: %s\n%s" % (float_format(a1_32), float_format(a2_32), type(a2_32)))
 print "\n"
+
+# Soluzione equazione
+def y32(n):
+    return  a1_32 * np.float32((1+sqrt(5))/2) ** n - a2_32 * np.float32((1-sqrt(5))/2) ** n
+
 
 n_max = 50
 plot_32 = [abs(y32(x)) for x in range(n_max)]
@@ -60,5 +56,5 @@ plt.grid(**GRID_OPTIONS)
 plt.legend(loc='upper center')
 plt.xlabel('n')
 plt.ylabel('$\log_{10} |y_n|$')
-plt.savefig('./figs/02-fibonacci.eps', dpi=SAVE_FIG_DPI)
-#plt.show()
+plt.savefig('./figs/02-fibonacci-pert.eps', dpi=SAVE_FIG_DPI)
+plt.show()
